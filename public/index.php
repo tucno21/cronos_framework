6421dda07ab3a5bc63f94959620ae16a94555476
tucno21 <carlitostucno@gmail.com>
2023-05-30 12:13:02 -0500

    cambio en ruta de autoload

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
-require_once __DIR__ . "./../vendor/autoload.php";
+// require_once __DIR__ . "./../vendor/autoload.php";
+require_once "./../vendor/autoload.php";
 
 Cronos\App::bootstrap(dirname(__DIR__))->run();
