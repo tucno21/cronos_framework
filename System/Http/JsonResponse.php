448bcc34fc7960a8b3a44f03f6f2f7c14458cda5
tucno21 <carlitostucno@gmail.com>
2023-11-08 10:35:32 -0500

    modificacion en la detecction de objetos con toArray

--- System/Http/JsonResponse.php
+++ System/Http/JsonResponse.php
@@ -17,10 +17,23 @@ class JsonResponse
         $data = match (true) {
             is_object($this->dataForJson) && method_exists($this->dataForJson, 'toArray') => $this->dataForJson->toArray(),
             is_object($this->dataForJson) && !method_exists($this->dataForJson, 'toArray') => (array) $this->dataForJson,
-            is_array($this->dataForJson) => $this->dataForJson,
+            is_array($this->dataForJson) => $this->handleNestedArrays($this->dataForJson),
             default => $this->dataForJson
         };
 
         return $data;
+    }
+
+    protected function handleNestedArrays(array $array)
+    {
+        foreach ($array as $key => $value) {
+            if (is_object($value) && method_exists($value, 'toArray')) {
+                $array[$key] = $value->toArray();
+            } elseif (is_array($value)) {
+                $array[$key] = $this->handleNestedArrays($value);
+            }
+        }
+
+        return $array;
     }
 }
