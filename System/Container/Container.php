1b8b82426829be185944e91a81b852c1e4cef612
tucno21 <carlitostucno@gmail.com>
2025-06-17 06:45:05 -0500

    Inyección de Dependencias y Contenedor con capacidades de autowiring

--- System/Container/Container.php
+++ System/Container/Container.php
@@ -2,6 +2,8 @@
 
 namespace Cronos\Container;
 
+use Cronos\Container\DependencyInjection;
+
 class Container
 {
     //propiedad para almacenar instancias de una clase y sea unica
@@ -10,14 +12,14 @@ class Container
     //metodo para crear una instancia de una clase
     //$class = nombre de la clase
     //$build = nombre de la clase o funcion anonima para crear la instancia
-    public static function singleton(string $class, string|callable|null $build = null)
+    public static function singleton(string $class, string|callable|null $build = null): object
     {
         //si no existe la clase en el array de instancias
         if (!array_key_exists($class, self::$instances)) {
             //match 
             match (true) {
-                is_null($build) => self::$instances[$class] = new $class(),
-                is_string($build) => self::$instances[$class] = new $build(),
+                is_null($build) => self::$instances[$class] = DependencyInjection::resolveClass($class),
+                is_string($build) => self::$instances[$class] = DependencyInjection::resolveClass($build),
                 is_callable($build) => self::$instances[$class] = $build(),
             };
         }
@@ -25,8 +27,31 @@ class Container
         return self::$instances[$class];
     }
 
-    public static function resolve(string $class)
+    /**
+     * Resuelve una instancia de una clase del contenedor.
+     * Si no existe como singleton, intenta resolverla usando autowiring.
+     *
+     * @param string $class El nombre de la clase a resolver.
+     * @return object La instancia resuelta de la clase.
+     */
+    public static function resolve(string $class): object
     {
-        return self::$instances[$class] ?? null;
+        if (self::has($class)) {
+            return self::$instances[$class];
+        }
+
+        // Si no es un singleton, intentar resolverla con autowiring
+        return DependencyInjection::resolveClass($class);
+    }
+
+    /**
+     * Verifica si una clase ya tiene una instancia singleton en el contenedor.
+     *
+     * @param string $class El nombre de la clase.
+     * @return bool True si la instancia existe, false en caso contrario.
+     */
+    public static function has(string $class): bool
+    {
+        return array_key_exists($class, self::$instances);
     }
 }
--- System/Container/DependencyInjection.php
+++ System/Container/DependencyInjection.php
@@ -9,9 +9,110 @@ use Cronos\Model\Model;
 use ReflectionFunction;
 use Cronos\Container\Container;
 use Cronos\Errors\HttpNotFoundException;
+use ReflectionUnionType;
+use ReflectionNamedType;
 
 class DependencyInjection
 {
+    /**
+     * Resuelve una clase y sus dependencias de forma recursiva.
+     *
+     * @param string $class El nombre de la clase a resolver.
+     * @return object La instancia resuelta de la clase.
+     * @throws \ReflectionException Si la clase no existe o no se puede instanciar.
+     */
+    public static function resolveClass(string $class): object
+    {
+        // Si la clase ya es un singleton en el contenedor, la retornamos.
+        if (Container::has($class)) {
+            return Container::resolve($class);
+        }
+
+        $reflector = new ReflectionClass($class);
+
+        // Si la clase no es instanciable (ej. interfaz, clase abstracta), lanzamos una excepción.
+        if (!$reflector->isInstantiable()) {
+            throw new \ReflectionException("Class {$class} is not instantiable.");
+        }
+
+        $constructor = $reflector->getConstructor();
+
+        // Si no hay constructor, simplemente instanciamos la clase.
+        if (is_null($constructor)) {
+            return new $class();
+        }
+
+        // Resolvemos las dependencias del constructor.
+        $dependencies = self::resolveDependencies($constructor->getParameters());
+
+        // Instanciamos la clase con sus dependencias.
+        return $reflector->newInstanceArgs($dependencies);
+    }
+
+    /**
+     * Resuelve las dependencias para un conjunto de parámetros.
+     *
+     * @param array $parameters Array de ReflectionParameter.
+     * @return array Array de dependencias resueltas.
+     * @throws \ReflectionException Si una dependencia no puede ser resuelta.
+     */
+    protected static function resolveDependencies(array $parameters): array
+    {
+        $dependencies = [];
+
+        foreach ($parameters as $parameter) {
+            $type = $parameter->getType();
+
+            // Si el parámetro no tiene tipo o es un tipo primitivo, intentamos resolverlo.
+            if (is_null($type) || $type->isBuiltin()) {
+                if ($parameter->isDefaultValueAvailable()) {
+                    $dependencies[] = $parameter->getDefaultValue();
+                } else {
+                    // Podríamos lanzar una excepción o intentar resolver desde algún otro lugar (ej. Request).
+                    // Por ahora, si es primitivo y no tiene valor por defecto, lo dejamos como null o lanzamos error.
+                    // Para autowiring básico, asumimos que las dependencias de objetos son clases.
+                    throw new \ReflectionException("Unresolvable dependency: {$parameter->getName()}");
+                }
+            } elseif ($type instanceof ReflectionNamedType) {
+                $className = $type->getName();
+                if ($type->isBuiltin()) {
+                    // Esto ya debería estar cubierto por el if anterior, pero por seguridad.
+                    if ($parameter->isDefaultValueAvailable()) {
+                        $dependencies[] = $parameter->getDefaultValue();
+                    } else {
+                        throw new \ReflectionException("Unresolvable builtin dependency: {$parameter->getName()}");
+                    }
+                } else {
+                    // Recursivamente resolvemos la dependencia de la clase.
+                    $dependencies[] = self::resolveClass($className);
+                }
+            } elseif ($type instanceof ReflectionUnionType) {
+                // Manejar tipos de unión (ej. ClassA|ClassB). Por simplicidad, intentaremos resolver el primer tipo que sea una clase.
+                $resolved = false;
+                foreach ($type->getTypes() as $unionType) {
+                    if ($unionType instanceof ReflectionNamedType && !$unionType->isBuiltin()) {
+                        try {
+                            $dependencies[] = self::resolveClass($unionType->getName());
+                            $resolved = true;
+                            break;
+                        } catch (\ReflectionException $e) {
+                            // Intentar con el siguiente tipo en la unión
+                        }
+                    }
+                }
+                if (!$resolved) {
+                    if ($parameter->isDefaultValueAvailable()) {
+                        $dependencies[] = $parameter->getDefaultValue();
+                    } else {
+                        throw new \ReflectionException("Unresolvable union type dependency: {$parameter->getName()}");
+                    }
+                }
+            }
+        }
+
+        return $dependencies;
+    }
+
     public static function resolveParameters(Closure|array $callback, $routeParameters = [])
     {
         $methodOrFunction = is_array($callback)
@@ -23,45 +124,83 @@ class DependencyInjection
         //recorrer los parametros de la funcion o metodo de la clase que se esta ejecutando
         foreach ($methodOrFunction->getParameters() as $param) {
             $resolved = null;
+            $type = $param->getType();
 
-            //is_subclass_of verifica si la clase que viene del parametro del metodo Controller es una subclase de Model
-            if (is_subclass_of($param->getType()->getName(), Model::class)) {
-                //instanciar la clase del parametro
-                $modelClass = new ReflectionClass($param->getType()->getName());
+            if (is_null($type)) {
+                // Si no hay tipo, intentar resolver desde los parámetros de ruta o valor por defecto
+                if (array_key_exists($param->getName(), $routeParameters)) {
+                    $resolved = $routeParameters[$param->getName()];
+                } elseif ($param->isDefaultValueAvailable()) {
+                    $resolved = $param->getDefaultValue();
+                } else {
+                    // No se pudo resolver, lanzar excepción o manejar de otra forma
+                    throw new \ReflectionException("Unresolvable dependency: {$param->getName()}");
+                }
+            } elseif ($type->isBuiltin()) { // verificar si el parametro es de tipo primitivo
+                // buscar el parametro en el array de parametros de la ruta
+                $resolved = $routeParameters[$param->getName()] ?? null;
+                if (is_null($resolved) && $param->isDefaultValueAvailable()) {
+                    $resolved = $param->getDefaultValue();
+                }
+            } elseif ($type instanceof ReflectionNamedType) {
+                $className = $type->getName();
+                //is_subclass_of verifica si la clase que viene del parametro del metodo Controller es una subclase de Model
+                if (is_subclass_of($className, Model::class)) {
+                    //separamos la clase por \
+                    $arrayClass = explode('\\', $className);
+                    //obtenemos el ultimo elemento del array
+                    $nameClass = end($arrayClass);
+                    //cambiamo todo a minusculas EN NOMBRE DE LA CLASE QUE VIENE DEL PARAMETRO DEL CONTROLADOR
+                    $keyParam = strtolower($nameClass);
 
-
-                //separamos la clase por \
-                $arrayClass = explode('\\', $param->getType()->getName());
-                //obtenemos el ultimo elemento del array
-                $nameClass = end($arrayClass);
-                //cambiamo todo a minusculas EN NOMBRE DE LA CLASE QUE VIENE DEL PARAMETRO DEL CONTROLADOR
-                $keyParam = strtolower($nameClass);
-
-                if (is_string($routeParameters[$keyParam])) {
-                    $id = $routeParameters[$keyParam];
-                    //buscamos el valor del parametro que viene de la ruta
-                    //ejecutamos el metodo find del modelo
-                    $resolved = $param->getType()->getName()::find($id ?? 0);
-                    if (is_null($resolved)) {
-                        throw new HttpNotFoundException();
+                    if (isset($routeParameters[$keyParam])) {
+                        if (is_string($routeParameters[$keyParam])) {
+                            $id = $routeParameters[$keyParam];
+                            //buscamos el valor del parametro que viene de la ruta
+                            //ejecutamos el metodo find del modelo
+                            $resolved = $className::find($id ?? 0);
+                            if (is_null($resolved)) {
+                                throw new HttpNotFoundException();
+                            }
+                        } else {
+                            //obtener la clave de $routeParameters[$keyParam]
+                            $column = array_key_first($routeParameters[$keyParam]);
+                            //obtener el valor de $routeParameters[$keyParam]
+                            $value = $routeParameters[$keyParam][$column];
+                            //ejecutamos el metodo where del modelo
+                            $resolved = $className::where($column, $value)->first();
+                            if (is_null($resolved)) {
+                                throw new HttpNotFoundException();
+                            }
+                        }
+                    } else {
+                        // Si no hay parámetro de ruta para el modelo, intentar resolverlo como una clase normal
+                        $resolved = self::resolveClass($className);
                     }
                 } else {
-                    //obtener la clave de $routeParameters[$keyParam]
-                    $column = array_key_first($routeParameters[$keyParam]);
-                    //obtener el valor de $routeParameters[$keyParam]
-                    $value = $routeParameters[$keyParam][$column];
-                    //ejecutamos el metodo where del modelo
-                    $resolved = $param->getType()->getName()::where($column, $value)->first();
-                    if (is_null($resolved)) {
-                        throw new HttpNotFoundException();
+                    //instanciar la clase del parametro usando el nuevo método de resolución
+                    $resolved = self::resolveClass($className);
+                }
+            } elseif ($type instanceof ReflectionUnionType) {
+                // Manejar tipos de unión en parámetros de ruta, similar a resolveDependencies
+                $resolved = false;
+                foreach ($type->getTypes() as $unionType) {
+                    if ($unionType instanceof ReflectionNamedType && !$unionType->isBuiltin()) {
+                        try {
+                            $resolved = self::resolveClass($unionType->getName());
+                            break;
+                        } catch (\ReflectionException $e) {
+                            // Intentar con el siguiente tipo en la unión
+                        }
                     }
                 }
-            } else if ($param->getType()->isBuiltin()) { //verificar si el parametro es de tipo primitivo
-                //buscar el parametro en el array de parametros de la ruta
-                $resolved = $routeParameters[$param->getName()] ?? null;
-            } else {
-                //instanciar la clase del parametro
-                $resolved = Container::singleton($param->getType()->getName());
+                if (!$resolved) {
+                    if ($param->isDefaultValueAvailable()) {
+                        $resolved = $param->getDefaultValue();
+                    } else {
+                        throw new \ReflectionException("Unresolvable union type dependency for parameter: {$param->getName()}");
+                    }
+                }
             }
 
             $params[] = $resolved;
